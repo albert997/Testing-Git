8819d47ad0252236205549e36b6e5fdf096c7e9b
Albert H <albert.hughes@exeterschool.devon.sch.uk>
2022-10-01 12:58:10 +0100

    ...

--- myapp/app.js
+++ myapp/app.js
@@ -3,8 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    // return the index.html file
+    // return index.html
     res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html')
+    
+
 })
 
 app.listen(port, () => {
