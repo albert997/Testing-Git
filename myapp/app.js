bcbfef3d0a2fddd8a1791355e884e75cb1978b17
Albert H <albert.hughes@exeterschool.devon.sch.uk>
2022-10-01 12:37:01 +0100

    globe attempt 2

--- myapp/app.js
+++ myapp/app.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    // return the BasicExample.html file
+    res.sendFile(__dirname + '/BasicExample.html');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
