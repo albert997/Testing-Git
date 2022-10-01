0d8f941c0f7eb75dffd9963ea98c9b042cdaecfd
Albert H <albert.hughes@exeterschool.devon.sch.uk>
2022-10-01 12:59:09 +0100

    required files

--- myapp/app.js
+++ myapp/app.js
@@ -4,10 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => {
     // return index.html
-    res.sendFile(__dirname + '/index.html')
-    res.sendFile(__dirname + '/index.html')
-    
-
+    res.sendFile(__dirname + '/index.html')  
 })
 
 app.listen(port, () => {
