36487ab014d1ff2e55a46b1a51eeaeb8c84f2817
Albert H <albert.hughes@exeterschool.devon.sch.uk>
2022-10-01 12:41:01 +0100

    first sucessful globe

--- myapp/app.js
+++ myapp/app.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    // return the BasicExample.html file
-    res.sendFile(__dirname + '/BasicExample.html');
+    // return the index.html file
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(port, () => {
